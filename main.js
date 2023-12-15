c17c1e0632cd6861d332a3c0f46fcf4dd4064b17
lublubczyk <lubczyk@ukr.net>
2023-12-15 13:55:27 +0200

    my connect

---
+++ main.js
@@ -0,0 +1,15 @@
+const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
+//[["bat"],["nat","tan"],["ate","eat","tea"]]
+const groupAnagrams = function (strs) {
+    const result = strs.reduce((acum, val) => {
+        const key = val.split('').sort().join('');
+        if (!acum[key]) acum[key] = [];
+        
+        acum[key].push(val);
+        return acum
+    },{});
+    return Object.values(result)
+};
+
+
+console.log(groupAnagrams(strs))
